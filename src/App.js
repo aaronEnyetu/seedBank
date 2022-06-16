8e2b3685b56cf51c9e8871957cd60b84a623152e
Aaron Enyetu <aaronenyetu@gmail.com>
2022-06-15 18:11:42 -0700

    Set State for components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+//Track state for allSeedPackets, filteredSeedPackets, seedFormPlantName, seedFormVariety, seedFormYearHarvested, seedFormGardener, seedFormPacketColor
+//Pass state as props correctly to SeedPacketForm, SeedPacket, and SeedPacketList
+//Define a handleDeleteSeedPacket function that deletes a seed packet from the state array using name
+//define a handleFilterSeedPacket function that takes in a string and set filterSeedPackets to an array of seed packets whose name matches that string
+
 import './App.css';
+import { useSate, useEffect } from 'react';
+import SeedPacketForm from './SeedPacketForm';
+import SeedPacketList from './SeedPacketList';
+import SeedPacket from './SeedPacket';
+import { useState } from 'react/cjs/react.production.min';
+
+
 
 function App() {
+  const [allSeedPackets, setAllSeedPackets] = useSate([]);
+  const [filteredSeedPackets, setAllFilteredSeedPackets] = useSate([]);
+  const [seedFormPlantName, setSeedFormPlantName] = useSate([]);
+  const [seedFormVariety, setSeedFormVariety] = useSate([]);
+  const [seedFormYearHarvested, setSeedFormYearHarvested] = useSate('');
+  const [seedFormGardener, setSeedFormGardener] = useSate('');
+  const [seedFormPacketColor, setSeedFormPacketColor] = useState('lightbrown');
+  const [filterQuery, setFilterQuery] = useSate('');
+
+  useEffect(() => handleFilterSeedPackets(filterQuery), [allSeedPackets, filterQuery]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
